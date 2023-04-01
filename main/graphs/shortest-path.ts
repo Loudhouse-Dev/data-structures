c5c9cf56fd922242f3b2e63bce217d21a584a29b
John <contactme@johnnyhowell.dev>
2023-04-01 15:37:21 -0500

    4/1

---
+++ main/graphs/shortest-path.ts
@@ -0,0 +1,22 @@
+/*
+Write a function, shortestPath, that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). 
+The function should return the length of the shortest path between A and B. 
+Consider the length as the number of edges in the path, not the number of nodes. If there is no path between A and B, then return -1.
+
+Ex.
+
+const edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v'],
+]
+
+*/
+
+export const shortestPath = (
+    edges: string[][],
+    nodeA: string,
+    nodeB: string
+) => {};
