1dab5606eb876247869026c01c60a742982cd84f
Loudhouse-Dev <contactme@johnnyhowell.dev>
2023-04-05 12:48:32 -0500

    cleaned up island-count algo

--- main/graphs/island-count.ts
+++ main/graphs/island-count.ts
@@ -16,25 +16,49 @@ example:
 */
 
 export function islandCount(grid: string[][]) {
+    const visited = new Set();
     let islandCount = 0;
+
+    //loop through the grid
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[row].length; col++) {
-            if (grid[row][col] === 'L') {
-                islandCount++;
-                sinkIsland(grid, row, col);
-            }
+            //if we find land and haven't visited it before, we have found an island
+            if (exploreIsland(grid, row, col, new Set()) === true)
+                islandCount += 1;
         }
     }
     return islandCount;
 }
 
-function sinkIsland(grid: string[][], row: number, col: number) {
-    if (row < 0 || row >= grid.length || col < 0 || col >= grid[row].length)
-        return;
-    if (grid[row][col] !== 'L') return;
-    grid[row][col] = 'D';
-    sinkIsland(grid, row - 1, col);
-    sinkIsland(grid, row + 1, col);
-    sinkIsland(grid, row, col - 1);
-    sinkIsland(grid, row, col + 1);
+function exploreIsland(
+    grid: string[][],
+    row: number,
+    col: number,
+    visited: Set<string>
+) {
+    //check if we're in bounds
+    const rowInbounds = 0 <= row && row < grid.length;
+    const colInbounds = 0 <= col && col < grid.length;
+
+    //escape if we're out of bounds
+    if (!rowInbounds || !colInbounds) return;
+    //we only want to traverse through land, no water
+    if (grid[row][col] === 'W') return;
+
+    const pos = row + ',' + col;
+    //if we've seen this node before, we don't want to traverse it again
+    if (visited.has(pos)) return false;
+    //add the node to the visited set
+    visited.add(pos);
+
+    //move up
+    exploreIsland(grid, row - 1, col, visited);
+    //move down
+    exploreIsland(grid, row + 1, col, visited);
+    //move left
+    exploreIsland(grid, row, col - 1, visited);
+    //mover right
+    exploreIsland(grid, row, col + 1, visited);
+
+    return true;
 }
