a1cceb83d14ae1b39b0ccd0a53d848f02c213de8
Loudhouse-Dev <contactme@johnnyhowell.dev>
2023-04-04 22:30:57 -0500

    another one

--- main/dynamic-programming/max-sum-increasing-subsequence.ts
+++ main/dynamic-programming/max-sum-increasing-subsequence.ts
@@ -16,22 +16,30 @@ export function maxSumIncreasingSubsequence(
     array: number[]
 ): [number, number[]] {
     const sequences: number[] = new Array(array.length);
+    //we need to initialize the sums array with the values of the original array
     const sums: number[] = array.map((num) => num);
+    //keep track of the index of the max sum
     let maxSumIdx = 0;
+    //iterate through the array twice
     for (let i = 0; i < array.length; i++) {
         const currentNum = array[i];
+        //iterate through the array again, but this time we only go up to the current index
         for (let j = 0; j < i; j++) {
+            //update sums array if the current number is greater than the other number
             const otherNum = array[j];
             if (otherNum < currentNum && sums[j] + currentNum >= sums[i]) {
                 sums[i] = sums[j] + currentNum;
                 sequences[i] = j;
             }
         }
+        //update the max sum index if the current sum is greater than the max sum
         if (sums[i] >= sums[maxSumIdx]) maxSumIdx = i;
     }
+    //return the max sum and the sequence
     return [sums[maxSumIdx], constructSequence(array, sequences, maxSumIdx)];
 }
 
+//helper function to construct the sequence
 function constructSequence(
     array: number[],
     sequences: number[],
