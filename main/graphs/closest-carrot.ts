a4a2c90c9ac2a572fbd6d7b5bbeef53985ee9611
Loudhouse-Dev <contactme@johnnyhowell.dev>
2023-04-06 12:43:13 -0500

    4/6

--- main/graphs/closest-carrot.ts
+++ main/graphs/closest-carrot.ts
@@ -26,6 +26,7 @@ export const closestCarrot = (
     colStart: number
 ) => {
     //keep track of visited nodes
+    //need to initialize with the starting node
     const visited = new Set([rowStart + ',' + colStart]);
     //we're using breadth-first search, so we need a queue
     const queue = [[rowStart, colStart, 0]];
@@ -34,38 +35,39 @@ export const closestCarrot = (
     while (queue.length > 0) {
         //destructure the queue
         const [row, col, distance] = queue.shift()!;
-        //if we've already visited this node, we don't want to traverse it again
-        //we represent the node as a string so we don't get tripped up on referential value
-        const position = row + ',' + col;
-        //if we've seen this node before, we don't want to traverse it again
-        if (visited.has(position)) continue;
-        //add the node to the visited set
-        visited.add(position);
-        //if there is no carrot, return the distance
+
+        //if there is a carrot, return the distance
         if (grid[row][col] === 'C') return distance;
-        //use a helper function to traverse through node neighbors
-        const neighbors = getNeighbors(grid, row, col);
-        for (const neighbor of neighbors) {
-            //add the neighbors to the queue
-            queue.push([neighbor[0], neighbor[1], distance + 1]);
+        const directions = [
+            [1, 0],
+            [0, 1],
+            [-1, 0],
+            [0, -1],
+        ];
+        for (let direction of directions) {
+            const [rowDir, colDir] = direction;
+            const rowNext = row + rowDir;
+            const colNext = col + colDir;
+            const rowInbounds = 0 <= rowNext && rowNext < grid.length;
+            const colInbounds = 0 <= colNext && colNext < grid[0].length;
+            //if we've already visited this node, we don't want to traverse it again
+            //we represent the node as a string so we don't get tripped up on referential value
+            const position = rowNext + ',' + colNext;
+            //if we've seen this node before, we don't want to traverse it again
+            if (visited.has(position)) continue;
+            //add the node to the visited set
+            visited.add(position);
+            //if we're out of bounds in either direction, we don't want to traverse it
+            if (!rowInbounds || !colInbounds) continue;
+            //if we're on a wall, we don't want to traverse it
+            if (grid[rowNext][colNext] === 'X') continue;
+            //add the node to the queue
+            if (!visited.has(rowNext + ',' + colNext)) {
+                queue.push([rowNext, colNext, distance + 1]);
+                visited.add(position);
+            }
         }
     }
     //if we've traversed the entire grid and there is no carrot, return -1
     return -1;
-};
-//helper function to get the neighbors of a node
-const getNeighbors = (grid: string[][], row: number, col: number) => {
-    const neighbors = [];
-    //check if the neighbor is in bounds and not a wall
-    //if neighbor above is in bounds and not a wall, add it to the neighbors array
-    if (row > 0 && grid[row - 1][col] !== 'X') neighbors.push([row - 1, col]);
-    //if neighbor below is in bounds and not a wall, add it to the neighbors array
-    if (row < grid.length - 1 && grid[row + 1][col] !== 'X')
-        neighbors.push([row + 1, col]);
-    //if neighbor to the left is in bounds and not a wall, add it to the neighbors array
-    if (col > 0 && grid[row][col - 1] !== 'X') neighbors.push([row, col - 1]);
-    //if neighbor to the right is in bounds and not a wall, add it to the neighbors array
-    if (col < grid[0].length - 1 && grid[row][col + 1] !== 'X')
-        neighbors.push([row, col + 1]);
-    return neighbors;
 };
